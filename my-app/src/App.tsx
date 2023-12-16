62a42d27ed10f71befa07bc0a4e8fd20d7dd1345
izuvell1 <isaiasprio1@gmail.com>
2023-12-16 04:54:21 +0100

    Struktur och react router dom

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Booking1 from './components/Booking1';
 import Booking2 from './components/Booking2';
+import { Outlet } from 'react-router';
 
 const App: React.FC = () => {
   return (
     <div>
       <h1>Världens Bästa Tennis Klubb</h1>
+      <Outlet/>
       <center><Booking1 /></center>
       <center><Booking2 /></center>
     </div>
--- my-app/src/Pages/Home/Home.tsx
+++ my-app/src/Pages/Home/Home.tsx
@@ -9,7 +9,7 @@ export default class Home extends Component<Props, State> {
 
   render() {
     return (
-      <div>Home</div>
+      <div className=''>Home</div>
     )
   }
 }
